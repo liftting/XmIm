a71a9526bb66b3277cc97d1269007a36961321bb
liftting <458286096@qq.com>
2015-05-05 12:59:34 +0800

    init

---
+++ XmApplication/app/src/main/java/com/xm/im/MainActivity.java
@@ -0,0 +1,18 @@
+package com.xm.im;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wm on 15/5/5.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.layer_main);
+
+    }
+}
